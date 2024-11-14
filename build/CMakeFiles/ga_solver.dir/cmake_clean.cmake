8d764b9831c5b56ad8b210cea7a16efb966c5c49
Harishjitu Saseendran <hsaseendran@uri.edu>
2024-11-14 11:38:00 -0500

    Initial Commit

---
+++ build/CMakeFiles/ga_solver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ga_solver.dir/src/main.cpp.o"
+  "CMakeFiles/ga_solver.dir/src/main.cpp.o.d"
+  "ga_solver"
+  "ga_solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ga_solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "GA Solver Build Setup Complete!" << std::endl;
+    return 0;
+}
